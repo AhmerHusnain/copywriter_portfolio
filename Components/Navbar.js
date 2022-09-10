fb97689f03f7f0c3b50d5ecd9ba1c04dfc2ce800
AhmerHusnain <chahmer143@gmail.com>
2022-09-10 06:26:05 +0500

    solved issues

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -5,9 +5,6 @@ import { useMediaQuery } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { userinfo } from "../Constants/userinfo";
-import Image from "next/image";
-
-import logo from "./../public/favicon.webp";
 
 const Navbar = ({ toggleTheme, currentTheme }) => {
   const [drawerVisible] = useMediaQuery("(max-width: 950px)");
@@ -27,65 +24,55 @@ const Navbar = ({ toggleTheme, currentTheme }) => {
   });
 
   return (
-    <>
-      <div className={styles.logo}>
-        <Link href="/">
-          <a>
-            <Image
-              src={logo}
-              alt=""
-              Layout="Responsive"
-              height="75"
-              width="150"
-            />
-          </a>
-        </Link>
-      </div>
+    <div
+      className={styles.navbar}
+      style={{
+        backgroundColor: currentTheme.secondary,
+        boxShadow: currentTheme.boxShadow,
+        position: sticky ? "fixed" : "static",
+      }}
+    >
       <div
-        className={styles.navbar}
         style={{
-          backgroundColor: currentTheme.secondary,
-          boxShadow: currentTheme.boxShadow,
-          position: sticky ? "fixed" : "static",
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "baseline",
+          marginBottom: !drawerVisible ? "0" : "10px",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            alignItems: "baseline",
-            marginBottom: !drawerVisible ? "0" : "10px",
-          }}
-        >
-          {!drawerVisible ? (
-            <div style={{ display: "flex" }}>
-              <Navlinks />
-            </div>
-          ) : null}
-          <Switch
-            id="dark-mode"
-            colorScheme="purple"
-            size={!drawerVisible ? "lg" : "md"}
-            isChecked={currentTheme.name === "dark" ? true : false}
-            onChange={() => toggleTheme()}
-          />
-        </div>
-        {drawerVisible ? (
-          <>
-            <hr></hr>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-evenly",
-                marginTop: "10px",
-              }}
-            >
-              <Navlinks />
-            </div>
-          </>
+        <Link href="https://www.fiverr.com/ahmerhusnain?public_mode=true">
+          <a>
+            <h2 className={styles.logo}>{userinfo.logoText}</h2>
+          </a>
+        </Link>
+        {!drawerVisible ? (
+          <div style={{ display: "flex" }}>
+            <Navlinks />
+          </div>
         ) : null}
+        <Switch
+          id="dark-mode"
+          colorScheme="purple"
+          size={!drawerVisible ? "lg" : "md"}
+          isChecked={currentTheme.name === "dark" ? true : false}
+          onChange={() => toggleTheme()}
+        />
       </div>
-    </>
+      {drawerVisible ? (
+        <>
+          <hr></hr>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              marginTop: "10px",
+            }}
+          >
+            <Navlinks />
+          </div>
+        </>
+      ) : null}
+    </div>
   );
 };
 
